76a4dac92a37e5efd6620ceeef5343d7f64cd398
hahsler <hahsler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-24 13:37:49 +0000

    score includes now initial state probability.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remm@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/score.R
+++ pkg/R/score.R
@@ -1,6 +1,7 @@
 ## does newdata come from the EMM?
 score <- function(emm, newdata, method = c("prod", "sum", "log_odds"), 
-    match_state="exact", transition_table = FALSE) {
+    initial_state_probability = FALSE, match_state="exact", 
+    transition_table = FALSE) {
 
     ## make sure  newdata is a matrix (maybe a single row)
     if(!is.matrix(newdata)) newdata <- as.matrix(rbind(newdata))
@@ -16,10 +17,24 @@ score <- function(emm, newdata, method = c("prod", "sum", "log_odds"),
     ## get probabilites
     from <- ssequence[1:(n-1)]
     to <- ssequence[2:n]
-    prob <- transition(emm, from, to)
     counts <- transition(emm, from, to, type="counts")
+    prob <- transition(emm, from, to)
     log_odds <- transition(emm, from, to, type="log_odds")
     
+    if(initial_state_probability) {
+        from <- c(NA, from)
+        to <- c(ssequence[1], to)
+        counts <- c(initial_transition(emm, type="counts")[ssequence[1]], 
+            counts)
+        prob <- c(initial_transition(emm)[ssequence[1]], 
+            prob)
+        log_odds <- c(initial_transition(emm, type="log_odds")[ssequence[1]], 
+            log_odds)
+        ## we have now one transition more
+        n <- n+1
+    }
+
+
     ## only transition table?
     if(transition_table) return(data.frame(from=from, to=to, prob=prob, 
             counts=counts))
